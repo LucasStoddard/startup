13b28a7d42b90ce0807d8b4c7fc5bf3458d698bd
LucasStoddard <lucasjohnstoddard@hotmail.com>
2024-11-07 15:31:44 -0700

    bug fix with routes, routes now added

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,16 +1,23 @@
+import React from "react";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
+function App() { //<Route exact path="/" component={Home}/>
   return (
     <div>
-      <Header />
-      <Home />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home/>} exact/>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
