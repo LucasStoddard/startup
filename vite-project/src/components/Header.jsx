2d158cc172964b2f674031c4676341aa7803c372
LucasStoddard <lucasjohnstoddard@hotmail.com>
2024-11-14 22:12:17 -0700

    name and logo creation

--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { NavLink, Link } from "react-router-dom";
 function Header() {
     return (
         <header>
-            <h1>PLANr</h1>
+            <h1><img src="/icon.png" alt="Icon" style={{ width: '45px', height: '45px' }}/>PLANr</h1>
             <h3>User's name</h3>
             <nav>
                 <NavLink to="/">Calendar</NavLink>
