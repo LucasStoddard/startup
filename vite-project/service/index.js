55201620ba0c086bf483aa30c91baab34ea60ab1
LucasStoddard <lucasjohnstoddard@hotmail.com>
2024-11-15 18:35:21 -0700

    pain

--- vite-project/service/index.js
+++ vite-project/service/index.js
@@ -1,4 +1,3 @@
-const port = process.argv.length > 2 ? process.argv[2] : 4000;
 const express = require('express');
 const app = express();
 
@@ -30,7 +29,8 @@ apiRouter.get('/weather', async (_req, res) => {
 app.use((_req, res) => {
     res.sendFile('index.html', { root: 'public' });
 });
-  
+
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
