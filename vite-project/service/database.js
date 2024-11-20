c376651aa05a9d87de4ed74e06e24f131e307397
LucasStoddard <lucasjohnstoddard@hotmail.com>
2024-11-19 17:32:07 -0700

    mongoDB implementation

---
+++ vite-project/service/database.js
@@ -0,0 +1,44 @@
+// I'm assuming this is supposed to be its own file? It only mentions this later and
+// doesn't mention it at all beforehand in the data services assignment
+// https://cloud.mongodb.com/v2/673d23a5fda2d97c4ed29b8e#/metrics/replicaSet/673d249b887b16391275ac26/explorer/rental/house/find
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+async function main() {
+  // Connect to the database cluster
+  const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+  const client = new MongoClient(url);
+  const db = client.db('rental');
+  const collection = db.collection('house');
+
+  // Test that you can connect to the database
+  (async function testConnection() {
+    await client.connect();
+    await db.command({ ping: 1 });
+  })().catch((ex) => {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  });
+
+  // Insert a document
+  const house = {
+    name: 'Beachfront views',
+    summary: 'From your bedroom to the beach, no shoes required',
+    property_type: 'Condo',
+    beds: 1,
+  };
+  await collection.insertOne(house);
+
+  // Query the documents
+  const query = { property_type: 'Condo', beds: { $lt: 2 } };
+  const options = {
+    sort: { score: -1 },
+    limit: 10,
+  };
+
+  const cursor = collection.find(query, options);
+  const rentals = await cursor.toArray();
+  rentals.forEach((i) => console.log(i));
+}
+
+main().catch(console.error);
