f4317195010ece1bf8d4aab108a7187bec8e519b
LucasStoddard <lucasjohnstoddard@hotmail.com>
2024-11-20 14:41:18 -0700

    login more progress

--- vite-project/service/database.js
+++ vite-project/service/database.js
@@ -2,43 +2,72 @@
 // doesn't mention it at all beforehand in the data services assignment
 // https://cloud.mongodb.com/v2/673d23a5fda2d97c4ed29b8e#/metrics/replicaSet/673d249b887b16391275ac26/explorer/rental/house/find
 const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
 const config = require('./dbConfig.json');
 
-async function main() {
-  // Connect to the database cluster
-  const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
-  const client = new MongoClient(url);
-  const db = client.db('rental');
-  const collection = db.collection('house');
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('startup');
+const userCollection = db.collection('user');
+const eventCollection = db.collection('event');
 
-  // Test that you can connect to the database
-  (async function testConnection() {
-    await client.connect();
-    await db.command({ ping: 1 });
-  })().catch((ex) => {
-    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
-    process.exit(1);
-  });
+// This will asynchronously test the connection and exit the process if it fails
+(async function testConnection() {
+  await client.connect();
+  await db.command({ ping: 1 });
+})().catch((ex) => {
+  console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+  process.exit(1);
+});
 
-  // Insert a document
-  const house = {
-    name: 'Beachfront views',
-    summary: 'From your bedroom to the beach, no shoes required',
-    property_type: 'Condo',
-    beds: 1,
+function getUser(email) {
+  return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function createUser(email, password) {
+  // Hash the password before we insert it into the database
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
   };
-  await collection.insertOne(house);
+  await userCollection.insertOne(user);
 
-  // Query the documents
-  const query = { property_type: 'Condo', beds: { $lt: 2 } };
+  return user;
+}
+
+// addScore -> addEvent
+// scoreCollection -> eventCollection
+async function addEvent(score) {
+  return eventCollection.insertOne(score);
+}
+
+// getHighScores -> getCalendar
+// This code DEFINITELY needs to change
+function getCalendar() {
+  const query = { score: { $gt: 0, $lt: 900 } };
   const options = {
     sort: { score: -1 },
     limit: 10,
   };
-
-  const cursor = collection.find(query, options);
-  const rentals = await cursor.toArray();
-  rentals.forEach((i) => console.log(i));
+  const cursor = eventCollection.find(query, options);
+  return cursor.toArray();
 }
 
-main().catch(console.error);
+// maybe add a clear scores here as well?
+
+// change score stuff here as well
+module.exports = {
+  getUser,
+  getUserByToken,
+  createUser,
+  addEvent,
+  getCalendar,
+};
--- vite-project/service/index.js
+++ vite-project/service/index.js
@@ -2,12 +2,20 @@ const express = require('express');
 // const cors = require('cors');
 // const fetch = require('node-fetch');
 const app = express();
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const DB = require('./database.js');
 
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
 // app.use(cors());
 
 app.use(express.static('public'));
 
 app.use(express.json());
+
+pp.use(cookieParser());
+
+app.set('trust proxy', true);
 
 var apiRouter = express.Router();
 // apiRouter.use(cors());
@@ -33,11 +41,80 @@ apiRouter.get('/weather', async (_req, res) => {
     }
 });
 
+// LOGIN STUFF
+apiRouter.post('/auth/create', async (req, res) => {
+    if (await DB.getUser(req.body.email)) {
+      res.status(409).send({ msg: 'Existing user' });
+    } else {
+      const user = await DB.createUser(req.body.email, req.body.password);
+      setAuthCookie(res, user.token);
+      res.send({
+        id: user._id,
+      });
+    }
+});
+
+apiRouter.post('/auth/login', async (req, res) => {
+    const user = await DB.getUser(req.body.email);
+    if (user) {
+      if (await bcrypt.compare(req.body.password, user.password)) {
+        setAuthCookie(res, user.token);
+        res.send({ id: user._id });
+        return;
+      }
+    }
+    res.status(401).send({ msg: 'Unauthorized' });
+});
+  
+apiRouter.delete('/auth/logout', (_req, res) => {
+    res.clearCookie(authCookieName);
+    res.status(204).end();
+});
+
+const secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+secureApiRouter.use(async (req, res, next) => {
+  const authToken = req.cookies[authCookieName];
+  const user = await DB.getUserByToken(authToken);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+});
+
+// getHighScores -> getCalendar
+// addScore -> addEvent
+// MODIFY TO BE getCalendar
+secureApiRouter.get('/scores', async (req, res) => {
+    const scores = await DB.getHighScores();
+    res.send(scores);
+});
+
+// MODIFY TO BE submitCalendar
+secureApiRouter.post('/score', async (req, res) => {
+    const score = { ...req.body, ip: req.ip };
+    await DB.addScore(score);
+    const scores = await DB.getHighScores();
+    res.send(scores);
+});
+  
+app.use(function (err, req, res, next) {
+    res.status(500).send({ type: err.name, message: err.message });
+});
+
 app.use((_req, res) => {
     res.sendFile('index.html', { root: 'public' });
 });  
 
-const port = process.argv.length > 2 ? process.argv[2] : 4000;
+function setAuthCookie(res, authToken) {
+    res.cookie(authCookieName, authToken, {
+      secure: true,
+      httpOnly: true,
+      sameSite: 'strict',
+    });
+  }
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
